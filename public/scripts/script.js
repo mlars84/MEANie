7a8ffb9911a416c4c567ca9a27a2d9d03b465e81
Matt Larson <matt.a.larson@gmail.com>
2017-05-10 14:54:37 -0500

    fixed formatting in script.js

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,28 +1,34 @@
-var myApp=angular.module( 'myApp', [] );
-myApp.controller( 'WhereMyPeeps', [ '$http', function( $http ){
+var myApp = angular.module( 'myApp', [] );
+
+myApp.controller( 'WhereMyPeeps', '$http', function( $http ){
+
 var vm = this;
+
+//function to add record
 vm.addRecord = function(){
-var objectToSend ={
-name= vm.nameIn,
-location= vm.locationIn,
-};
-$http({
-method: 'POST',
-url: '/testPost',
-data: objectToSend
-});
-vm.nameIn ='';
-vm.locationIn='';
-};
+  var objectToSend ={
+    name: vm.nameIn,
+    location: vm.locationIn
+  };
+  $http({
+    method: 'POST',
+    url: '/testPost',
+    data: objectToSend
+  });
+  vm.nameIn ='';
+  vm.locationIn='';
+}; //end addRecord
+
+//function to get record from DB
 vm.getRecords = function(){
-$.http({
-method: 'GET',
-url: '/getRecords',
-}).then( function( response ){
-vm.allTheRecords = response;
-console.log( vm.allTheRecords );
-}), function myError( response ){
-console.log( response.statusText );
-};
-};
-}]);
+  $http({
+    method: 'GET',
+    url: '/getRecords',
+  }).then( function( response ){
+    vm.allTheRecords = response;
+    console.log( vm.allTheRecords );
+  }).then( function myError(response) {
+    console.log(response.statusText);
+  });
+}; //end getRecords
+}); //end controller function
