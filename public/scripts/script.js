7fb956c530ae723d8b92ebb6beaee304ab7e5c9a
Matt Larson <matt.a.larson@gmail.com>
2017-11-07 10:56:34 -0600

    refactored to es8

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,50 +1,48 @@
-var myApp = angular.module( 'myApp', [] );
+angular.module( 'myApp', [] )
+  .controller( 'MeanieController', function( $http ){
+  const vm = this
 
-myApp.controller( 'WhereMyPeeps', function( $http ){
-
-var vm = this;
-
-//function to add record
-vm.addRecord = function(){
-  var objectToSend ={
-    name: vm.nameIn,
-    location: vm.locationIn
-  };
-  $http({
-    method: 'POST',
-    url: '/testPost',
-    data: objectToSend
-  });
-  vm.nameIn ='';
-  vm.locationIn='';
-}; //end addRecord
-
-//function to get record from DB
-vm.getRecords = function(){
-  $http({
-    method: 'GET',
-    url: '/getRecords',
-  }).then( function( response ){
-    vm.allTheRecords = response.data;
-    console.log( vm.allTheRecords );
-  }, function myError( response ){
-    console.log( response.statusText );
-    vm.getRecords();
-  });
-
-}; //end getRecords
-
-//function to delete a record from DOM and DB
-vm.delete = function(id){
-    console.log('delete', id);
+  //function to add record
+  vm.addRecord = () => {
+    let objectToSend = { 
+      name: vm.nameIn,
+      location: vm.locationIn
+    }
     $http({
-      method: 'DELETE',
-      url: '/deleteRecord/' + id,
-    }).then(function mySuccess(response){
-      vm.getRecords();
-    }, function myError(response){
-      console.log(response);
-    });
-  };
+      method: 'POST',
+      url: '/testPost',
+      data: objectToSend
+    })
+      vm.nameIn =''
+      vm.locationIn=''
+  } //end addRecord
 
-}); //end controller function
+  //function to get record from DB
+  vm.getRecords = () => {
+    $http({
+      method: 'GET',
+      url: '/getRecords',
+    }).then(res => {
+      vm.allTheRecords = res.data
+      console.log( vm.allTheRecords )
+    })
+    .catch(err => {
+      console.log(err)
+    })
+  } //end getRecords
+
+  //function to delete a record from DOM and DB
+  vm.delete = (id) => {
+      console.log('delete', id)
+      $http({
+        method: 'DELETE',
+        url: '/deleteRecord/' + id,
+      }).then(res => {
+        vm.getRecords()
+      })
+      .catch(err => {
+        console.log(err)
+      })
+    }
+
+}) //end controller function
--- server/app.js
+++ server/app.js
@@ -1,64 +1,65 @@
 //requires
-var express = require( 'express' );
-var app = express();
-var path = require( 'path' );
-var bodyParser = require( 'body-parser' );
-var mongoose = require( 'mongoose' );
+const express = require( 'express' )
+const app = express()
+const path = require( 'path' )
+const bodyParser = require( 'body-parser' )
+const mongoose = require( 'mongoose' )
+const PORT = process.env.PORT || 3333 
 
 //uses
-app.use( bodyParser.json() );
-app.use( express.static( 'public' ) );
-app.use(bodyParser.urlencoded({extended:true}));
+app.use( bodyParser.json() )
+app.use( express.static( 'public' ) )
+app.use(bodyParser.urlencoded({extended:true}))
 
 // 27017 is default mongo port
-mongoose.connect( 'localhost:27017/meanie' );
+mongoose.connect( 'localhost:27017/meanie' )
 
 //schema
-var ourSchema = mongoose.Schema({
-name: String,
-location: String
-});
+const ourSchema = mongoose.Schema({
+  name: String,
+  location: String
+})
 
 //model
-var ourModel = mongoose.model( 'ourmodels', ourSchema );
+const ourModel = mongoose.model( 'ourmodels', ourSchema )
 
 //base url hit
-app.get( '/', function( req, res ){
-  res.sendFile( path.resolve( 'public/index.html' ) );
-});
+app.get( '/', ( req, res ) => {
+  res.sendFile( path.resolve( 'public/index.html' ) )
+})
 
-app.get( '/getRecords', function( req, res ){
+app.get( '/getRecords', ( req, res ) => {
   // get and send back all the things
-  ourModel.find().then( function( data ){
-    res.send( data );
-  });
-}); //end getRecords GET
+  ourModel.find().then(data => {
+    res.send( data )
+  })
+}) //end getRecords GET
 
-app.post( '/testPost', function( req, res ){
-  console.log( 'req.body.name: ' + req.body.name );
+app.post( '/testPost', ( req, res ) => {
+  console.log( 'req.body.name: ' + req.body.name )
   // retrieved the req.body
   // putting it into an object to be saved in the db
-  var recordToAdd = {
+  let recordToAdd = {
     name: req.body.name,
     location: req.body.location
-  };
+  }
   // create new record
-  var newRecord = ourModel( recordToAdd );
-  newRecord.save();
-}); //end testPost POST
+  let newRecord = ourModel( recordToAdd )
+  newRecord.save()
+}) //end testPost POST
 
-app.delete('/deleteRecord/:id',function(req,res){
-  console.log('got this:',req.params.id);
-  ourModel.remove({_id: req.params.id},function(err){
+app.delete('/deleteRecord/:id', (req,res) => {
+  console.log('got this:',req.params.id)
+  ourModel.remove({_id: req.params.id}, (err) => {
     if(err){
-      console.log(err);
+      console.log(err)
     }
     else{
-      res.send(200);
+      res.send(200)
     }
-  });
-});
+  })
+})
 
-app.listen( 8080, 'localhost', function( req, res ){
-  console.log( 'listening on 8080' );
-});
+app.listen( PORT, 'localhost', ( req, res ) => {
+  console.log( `Listening on ${PORT}` )
+})
